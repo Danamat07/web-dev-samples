50b6f4ab49b0852197a9733fd370994a758d8552
Matei_Dana <marydanamat@gmail.com>
2025-03-09 16:40:19 +0200

    Remove test data

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -1,10 +1,4 @@
-const todoList = [{
-  name:'make dinner',
-  dueDate: '2025-12-22'
-}, { name:'wash dishes',
-dueDate: '2025-12-22'
-}];
-
+const todoList = [];
 renderTodoList();
 
 function renderTodoList() {
